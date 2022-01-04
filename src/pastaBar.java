9c2cc55769879ee48e54ffa23203e1d28ab231a5
Jovan Nikolic <jovannikolic@gmail.com>
2022-01-04 10:08:24 +0100

    Main program

--- src/pastaBar.java
+++ src/pastaBar.java
@@ -11,7 +11,47 @@ public class pastaBar {
 		String[] regularCustomers = { "0631111111", "0632222222", "0633333333", "0644444444", "0655555555",
 				"0666666666" };
 		int[] price = { 50, 60, 120, 120, 140, 100, 100, 100, 80, 50, 80, 80, 80, 50, 50, 20 };
+		int totalPrice=0;
+		int totalPriceDiscount=0;
+		System.out.println("**********Dobrodosli u Pasta Bar Izvolite!!**********");
+		System.out.println("*****************************************************");
+		System.out.println("***********Sami birate svoju idealnu pastu***********");
+		System.out.println("*****************************************************");
+		System.out.println("******Kada odaberete sastojke kucajte--Poruci--******");
+		System.out.println("*****************************************************");
+		String customerIngredient = "";
+		
+	
+		while(!customerIngredient.equalsIgnoreCase("poruci")) {
+			System.out.println("Izaberite sastojak");
+			customerIngredient=s.nextLine();
+			
+			System.out.println("*****************************************************");
+			
+			for(int i=0;i<price.length;i++) {
+				if (i==chosenIngredients(ingredients, customerIngredient)) {
+					totalPrice=totalPrice+price[i];
+				}
+			
+			}
 
+		}if (customerIngredient.equals("poruci")) {
+			System.out.println(">>>>>>>>>>>>Vasa porudzbina je zabelezena<<<<<<<<<<<<<");
+			System.out.println("Cena je " + totalPrice + " rsd");
+			System.out.println("******************************************************");
+			
+		}System.out.println("Unesite broj telefona");
+		String phoneNumber=s.next();
+			System.out.println("******************************************************");
+			if(isRegularCustomer(regularCustomers, phoneNumber)==true) {
+				totalPriceDiscount=totalPrice-(totalPrice*10/100);
+				System.out.println("Kao redovna musterija ostavrujete popust 10%");
+				System.out.println("Cena je " + totalPriceDiscount + " rsd");
+			}else {
+			System.out.println("Postanite nasa redovna musterija i ostvaricete popust od 10%");
+			}
+			System.out.println("*****************************************************");
+			System.out.println("******************Hvala Vam na poseti****************");
 	}
 
 	public static int chosenIngredients(String[] ingredients, String customerIngredient) {
